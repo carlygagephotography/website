b5bc33afd7db4437ce13648fa12f8af32281f178
Elliot <elliot@carlygagephotography.com>
2026-01-08 17:54:10 -0600

    Add Meta Pixel for Facebook tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,6 +129,32 @@ export default function RootLayout({
             gtag('config', 'G-4E5F8C8H7E');
           `}
         </Script>
+        
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1832786967436414');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1832786967436414&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
